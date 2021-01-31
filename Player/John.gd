cd5ec9f6676bf5a2f7bdb51c32429c897d4983a8
Leo Esc <leonardodanielescoto@gmail.com>
2021-01-30 22:07:34 -0600

    Add move to click func

--- Player/John.gd
+++ Player/John.gd
@@ -6,6 +6,7 @@ const FRICTION = 600
 
 var velocity = Vector2.ZERO
 var facing = "Front";
+var last_mouse_pos = null
 
 func _physics_process(_delta):
 	movement()
@@ -13,6 +14,7 @@ func _physics_process(_delta):
 
 func movement():
 	movementSmooth()
+	mouseMovement()
 	
 	# Controls player right and left animation sprites using arrow keys.
 	if !(Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_right")):
@@ -37,7 +39,7 @@ func movement():
 func movementSmooth():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
@@ -45,3 +47,20 @@ func movementSmooth():
 	else:
 		$AnimationPlayer.play("still" + facing)
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+
+# Moves character to where mouse clicked. 
+# Currently unfinished and wildy dysfunctional.
+# Please refer to 
+# https://www.youtube.com/watch?v=5bxys-Zo_jk&list=PLllc6qRBTEefSTIsPZVqhhuGNMc-5kOS6&index=16
+func _input(event):
+	if event.is_action_pressed("mouse_move"):
+		last_mouse_pos = get_viewport().get_mouse_position()
+
+func mouseMovement():
+	if last_mouse_pos:
+		var direction_vector = (last_mouse_pos - global_position)
+		
+		if direction_vector.length() < 10:
+			return 
+		
+		move_and_slide(direction_vector.normalized() * MAX_SPEED)
