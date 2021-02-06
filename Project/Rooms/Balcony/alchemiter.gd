3d95bf212f9e496e5b8a8e792465678b079b8ea9
Leo Esc <leonardodanielescoto@gmail.com>
2021-02-05 23:11:11 -0600

    Make alchemiter interactable sorta

---
+++ Project/Rooms/Balcony/alchemiter.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var dialog = load("res://UI/Dialog Box/DialogBox.gd").new()
+
+func _on_AlchemiterArea2D_mouse_entered()->void:
+	Input.set_custom_mouse_cursor(load("res://UI/cursor_select.png"),Input.CURSOR_ARROW,Vector2(14, 4))
+
+
+func _on_AlchemiterArea2D_mouse_exited()->void:
+	Input.set_custom_mouse_cursor(load("res://UI/cursor.png"))
+
+
+func _on_AlchemiterArea2D_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
