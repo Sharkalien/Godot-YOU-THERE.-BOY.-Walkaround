4cdd54ca8ff935dece9e02c711ac375f9e7e5ca6
Axollyon <Axollyon@users.noreply.github.com>
2021-07-13 20:55:40 -0400

    Player interaction and new click detection method

--- Project/Scripts/Interactable.gd
+++ Project/Scripts/Interactable.gd
@@ -6,33 +6,37 @@ export var width = 552;
 export var isWarp = false;
 export (String, MULTILINE) var dialogOrScene = "";
 
+var selected = false;
+
 var commandBox = load("res://UI/Command Box/Command_Player.tscn")
 
 func _on_mouse_entered()->void:
 	if (!Global.fading):
 		Global.hoverNodes.append(self);
+		selected = true;
 
 func _on_mouse_exited()->void:
 	if (!Global.fading):
 		Global.hoverNodes.erase(self);
+		selected = false;
 	
 func _exit_tree():
-	Global.hoverNodes.erase(self);
-
-func _on_input_event(viewport, event, _shape_idx):
-	if (Global.commandsNode && !Global.dialogOpen && !Global.fading && event is InputEventMouseButton && event.button_index == 1 && event.pressed == true):
+	Global.hoverNodes.erase(self);	
+	
+func _process(_delta):
+	if (Global.commandsNode && !Global.dialogOpen && !Global.fading && selected && Input.is_action_just_pressed("click")):
 		var commandBoxInstance = commandBox.instance();
 		Global.remove_commands();
 		Global.commandsNode.add_child(commandBoxInstance);
 		var click = get_global_mouse_position();
 		var cTrans = get_canvas_transform();
 		var cScale = cTrans.get_scale();
-		var right = (-cTrans.get_origin() / cScale + viewport.size / cScale).x;
+		var right = (-cTrans.get_origin() / cScale + get_viewport().size / cScale).x;
 		if (click.x + width > right):
 			click.x = right - width;
 		commandBoxInstance.global_position = click;
 		commandBoxInstance.command = command;
 		commandBoxInstance.get_node("CommandBox").rect_size.x = width;
-		commandBoxInstance.get_node("CommandBox/NinePatchRect/MarginContainer/RichTextLabel").bbcode_text = command;
+		commandBoxInstance.get_node("CommandBox/NinePatchRect/MarginContainer/RichTextLabel").bbcode_text = "";
 		commandBoxInstance.isWarp = isWarp;
 		commandBoxInstance.dialogOrScene = dialogOrScene;
