f0642b0feadb9d1804e5e33a67b9d103fc9a33be
Leo Esc <leonardodanielescoto@gmail.com>
2023-01-15 16:00:26 -0600

    Remove isWarp boolean
    The variable isn't needed anymore since the code checks if warpScene isn't null instead. A bit nicer removing the extra step

--- Project/Scripts/Resources/InteractResourceExtra.gd
+++ Project/Scripts/Resources/InteractResourceExtra.gd
@@ -1,7 +1,6 @@
 extends InteractDialog
 class_name InteractExtra
 
-export(bool) var isWarp = false
 export(String, FILE, "*.tscn,*.scn") var warpScene
 export(Vector2) var warpPos
 export(Texture) var zoomImage
