475bf3480cfda4a9226701c12687e7cf7eb11ff5
Leo Esc <leonardodanielescoto@gmail.com>
2023-05-31 19:40:42 -0500

    Make Kernelsprite not immediately change facing direction

--- Project/Kernelsprite/Kernelsprite.gd
+++ Project/Kernelsprite/Kernelsprite.gd
@@ -10,11 +10,11 @@ onready var playerScale = Global.playerNode.get_node("PlayerArea2D")
 
 func _physics_process(_delta: float) -> void:
 	spritePos.x = spritePos.x + (Global.playerNode.position.x - spritePos.x) / 60
-	self.position.x = spritePos.x
+	position.x = spritePos.x
 	spritePos.y += (Global.playerNode.position.y - spritePos.y) / 30
 	spriteFloat += 0.1
-	self.position.y = (spritePos.y + sin(spriteFloat) * 40) - 60
+	position.y = (spritePos.y + sin(spriteFloat) * 40) - 60
 	
-	if Global.playerNode.position.x != self.position.x:
-		self.scale.x = playerScale.scale.x
+	if Global.playerNode.position.x != position.x:
+		scale.x = scale.y * (abs(position.x - Global.playerNode.position.x) / (position.x - Global.playerNode.position.x))
 #	print(spriteFloat)
