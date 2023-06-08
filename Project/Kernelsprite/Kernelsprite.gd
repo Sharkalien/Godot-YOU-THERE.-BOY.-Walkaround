bb181e54f8e714783eeb59e05fdc35b38d06d75f
Leo Esc <leonardodanielescoto@gmail.com>
2023-06-08 17:09:48 -0500

    Tweak Kernelsprite
    Math's slightly different than the original Flash's, but looks and feels a lot closer now

--- Project/Kernelsprite/Kernelsprite.gd
+++ Project/Kernelsprite/Kernelsprite.gd
@@ -6,11 +6,11 @@ var spriteFloat:float
 
 
 func _physics_process(_delta: float) -> void:
-	spritePos.x = spritePos.x + (Global.playerNode.position.x - spritePos.x) / 60
+	spritePos.x = spritePos.x + (Global.playerNode.position.x - spritePos.x) / 40
 	position.x = spritePos.x
 	spritePos.y += (Global.playerNode.position.y - spritePos.y) / 30
-	spriteFloat += 0.1
-	position.y = (spritePos.y + sin(spriteFloat) * 40) - 60
+	spriteFloat += 0.06
+	position.y = (spritePos.y + sin(spriteFloat) * 40) - 90
 	
 	if Global.playerNode.position.x != position.x:
 		scale.x = scale.y * (abs(position.x - Global.playerNode.position.x) / (position.x - Global.playerNode.position.x))
