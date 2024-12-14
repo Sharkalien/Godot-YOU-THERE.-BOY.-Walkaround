9c4281b4b6f299f50ae4f080a737fb90a2f5e098
Sharkalien <leonardodanielescoto@gmail.com>
2024-12-14 02:51:05 -0600

    Fix TricksterHint label not working on mobile
    Mobile input is stupid, it triggers the event twice, therefore enabling and disabling trickster mode at once

--- Project/Rooms/Backyard/TricksterHint.gd
+++ Project/Rooms/Backyard/TricksterHint.gd
@@ -1,6 +1,6 @@
 extends RichTextLabel
 
 
-func _on_TricksterHint_gui_input(event):
-	if event.is_pressed():
+func _on_TricksterHint_gui_input(_event):
+	if Input.is_action_just_released("click"):
 		Signals.emit_signal("trickster")
